efed27ebd6fbd0384a971b24a8ddf20771868c57
Matt Wilson <mwilson82@hotmail.com>
2022-09-21 01:36:44 +0000

    Created assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function(array1, array2) {
+  let firstArray = JSON.stringify(array1)
+  let secondArray = JSON.stringify(array2)
+  if (firstArray === secondArray) {
+    return (true)
+  } else if (firstArray !== secondArray) {
+    return (false)
+  }
+}
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
+  } else if (eqArrays(array1, array2) === false) {
+    console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3])
+assertArraysEqual([1, 2, 3], [3, 2, 1])
+assertArraysEqual(['1', '2', '3'], ['1', '2', '3'])
+assertArraysEqual(['1', '2', '3'], ['1', '2', 3])
