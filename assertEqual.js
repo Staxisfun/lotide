7144b30950d858edc69a505ee7657bbccb476853
Matt Wilson <mwilson82@hotmail.com>
2022-09-26 20:37:18 +0000

    added test files for assertEqual and Head

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,4 @@
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
@@ -6,9 +7,8 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("hello", "hello");
-assertEqual("hello", "goodbye");
-assertEqual(19, 19);
-assertEqual(19, 19.19);
+
+
+
+
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual("hello", "goodbye");
+assertEqual(19, 19);
+assertEqual(19, 19.19);
